4b058b306f161ec4e4e0412acc30ef5d8c57048e
Sergey Slastnikov <codemonkey2263@gmail.com>
2022-02-22 23:43:57 +0400

    feat: reverse int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n)
+  let Array = String(n).split('');
+  let resultArray = Array.reverse();
+  let result = resultArray.join('');
+  return result*1;
 }
